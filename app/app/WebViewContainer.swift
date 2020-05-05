9198da58a3025739f0d31cb6a4b02f8a3bf20312
JackPu <kakashjack@gmail.com>
2020-05-05 17:50:18 +0800

    feat: add your web url

--- app/app/WebViewContainer.swift
+++ app/app/WebViewContainer.swift
@@ -10,18 +10,31 @@ import SwiftUI
 
 struct WebViewContainer: View {
     @State private var propSelect = false
+    @State private var url = "https://swift-webview-video.now.sh/"
+    @State private var inputUrl = ""
     var landmark: Landmark
     var body: some View {
         VStack {
             Text("WebView Test").font(.title)
             if (self.propSelect) {
-                WebView(request:  URLRequest(url: URL(string: "https://swift-webview-video.now.sh/")!))
+                WebView(request:  URLRequest(url: URL(string: self.url)!))
             } else {
-                EmptyWebview(request:  URLRequest(url: URL(string: "https://swift-webview-video.now.sh/")!))
+                EmptyWebview(request:  URLRequest(url: URL(string: self.url)!))
             }
             
             Spacer()
             VStack(alignment: .leading) {
+                TextField("Input your web url", text: $inputUrl)
+                Button(action: {
+                    if (self.verifyUrl(urlString: self.inputUrl)) {
+                        self.url = self.inputUrl
+                    } else {
+                        self.createAler()
+                    }
+                    
+                }) {
+                    Text("提交测试")
+                }
                 Toggle(landmark.name, isOn: $propSelect)
                 Text(landmark.desc)
             }
@@ -29,6 +42,17 @@ struct WebViewContainer: View {
             .frame(height: 200.0)
         }
     .navigationBarTitle(Text(landmark.name), displayMode: .inline)
+    }
+    func verifyUrl(urlString: String?) -> Bool {
+        if let urlString = urlString {
+            if let url = NSURL(string: urlString) {
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
+        }
+        return false
+    }
+    func createAler() -> Void {
+        Alert(title: Text("Important message"), message: Text("Wear sunscreen"), dismissButton: .default(Text("Got it!")))
     }
 }
 
